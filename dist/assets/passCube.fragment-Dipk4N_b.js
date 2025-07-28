c9557e80b7455e81c803678abfc276a275c06d33
godfatherc08 <edehchimaobi67@gmail.com>
2025-07-28 15:26:35 +0100

    "New sort of valley, updated physics"

--- dist/assets/passCube.fragment-Dipk4N_b.js
+++ dist/assets/passCube.fragment-Dipk4N_b.js
@@ -1,4 +1,4 @@
-import{G as r}from"./index-DjxoVRif.js";const e="passCubePixelShader",t=`varying vec2 vUV;uniform samplerCube textureSampler;
+import{G as r}from"./index-ByUZCAUT.js";const e="passCubePixelShader",t=`varying vec2 vUV;uniform samplerCube textureSampler;
 #define CUSTOM_FRAGMENT_DEFINITIONS
 void main(void) 
 {vec2 uv=vUV*2.0-1.0;
